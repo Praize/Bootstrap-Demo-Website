d792ca90a330809a346082e41cbd0a4724a69547
Praize <senemenyuswa07@gmail.com>
2022-04-07 20:41:55 +0200

    First upload of the website files

---
+++ Practice@seneme1/js/scrollReveal.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal({duration: 2000});
+
+sr.reveal('.site-content .d-flex');
+sr.reveal('.section-2 .card');
+sr.reveal('.section-3 .d-flex');
+sr.reveal('.section-4 .d-flex');
+sr.reveal('.section-5 .col-md-7, .col-md-5');
+sr.reveal('.section6 .form-inline , .d-flex , h4');
